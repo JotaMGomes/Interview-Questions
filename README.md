b70eed62e63571d30a5d5a840c7ad3bba5237034
JotaMGomes <jotamgomes@gmail.com>
2020-04-09 22:30:13 -0700

    project start

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Interview-Questions
 My solutions for some interview questions.
+
+## LeetCode
+
+**01** [Two Sum](https://leetcode.com/problems/two-sum/)
+
---
+++ src/LeetCode/LeetCode01.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LeetCode01 {
+
+	public static void main(String[] args) {
+
+		int vNums01[] = new int[]{2, 7, 11, 15};
+		int vTarget01 = 9;
+
+	}
+	
+	/*
+	 * tests:
+	 * >> twoSum[] is empty
+	 * 
+	 */
+	
+	class Solution {
+	    public int[] twoSum(int[] nums, int target) {
+	    	
+
+	        
+	    	int vResult[] = new int[]{0};
+	        return vResult;
+	    }
+	}
+
+}
