12c8aa7116ed68c47b2b98069438b4f429b08751
jotamgomes <jotamgomes@gmail.com>
2020-04-27 21:19:39 -0700

    Amazon04 v2

--- src/Amazon/Amazon04.java
+++ src/Amazon/Amazon04.java
@@ -3,7 +3,11 @@ package Amazon;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Amazon04 {
 
@@ -47,9 +51,49 @@ public class Amazon04 {
 				Arrays.asList(0,0,0,0,0),
 				Arrays.asList(0,0,0,0,0)
 		       );	
-			System.out.println("==========");
-			System.out.println(vSolution.updateServerGrid(grid, rows, columns));
-
+		System.out.println("==========");
+		System.out.println(vSolution.updateServerGrid(grid, rows, columns));
+			
+		System.out.println("==========");
+		System.out.println("==========");
+		System.out.println("==========");
+		
+		Solution04_a vSolution_a = new Solution04_a();
+		grid = Arrays.asList(
+				Arrays.asList(0,1,1,0,1),
+				Arrays.asList(0,1,0,1,0),
+				Arrays.asList(0,0,0,0,1),
+				Arrays.asList(0,1,0,0,0)
+		       );
+		System.out.println(vSolution_a.updateServerGrid_a(grid, rows, columns));
+		
+		grid = Arrays.asList(
+				Arrays.asList(1,1,1,1,1),
+				Arrays.asList(1,1,1,1,1),
+				Arrays.asList(1,1,1,1,1),
+				Arrays.asList(1,1,1,1,1)
+		       );
+		System.out.println("==========");
+		System.out.println(vSolution_a.updateServerGrid_a(grid, rows, columns));
+		
+		grid = Arrays.asList(
+			Arrays.asList(0,0,0,0,0),
+			Arrays.asList(0,0,0,0,0),
+			Arrays.asList(0,0,0,0,0),
+			Arrays.asList(0,0,0,0,0)
+	       );	
+		System.out.println("==========");
+		System.out.println(vSolution_a.updateServerGrid_a(grid, rows, columns));
+		
+		
+		grid = Arrays.asList(
+				Arrays.asList(1,0,0,0,0),
+				Arrays.asList(0,0,0,0,0),
+				Arrays.asList(0,0,0,0,0),
+				Arrays.asList(0,0,0,0,0)
+		       );	
+		System.out.println("==========");
+		System.out.println(vSolution_a.updateServerGrid_a(grid, rows, columns));
 	}
 
 }
@@ -154,3 +198,93 @@ class Solution04 {
 		}
 	}	
 }
+
+
+class Solution04_a {
+	
+	public int updateServerGrid_a(List<List<Integer>> grid, int rows, int columns) {
+		
+		// create a HashSet to store the indexes of zeros in the grid
+		HashSet<String> listZeros = new HashSet<String>();
+		
+		// initialize return variable numHours with 0
+		int numHours = 0;
+		
+		// flag to identify if at least one zero was found
+		boolean myFlag = false;
+		
+		// loop through rows
+		for(int r=0;r<rows;r++) {
+			// loop through columns
+			for(int c=0;c<columns;c++) {
+				if(grid.get(r).get(c)==0) {
+					// found a zero
+					myFlag=true;
+					if(!((c<columns-1 && grid.get(r).get(c+1)==1) || 
+						 (c>0         && grid.get(r).get(c-1)==1) ||
+						 (r<rows-1    && grid.get(r+1).get(c)==1) ||
+						 (r>0         && grid.get(r-1).get(c)==1))
+				      )
+					{
+						// keep key to delete from 
+						listZeros.add(r+"#"+c);
+					}
+				}
+			}
+		}
+		
+		if(!myFlag)
+			// all servers already have the file
+			return 0;
+		
+		// update number of hours
+		numHours++;
+		
+		// loop while there are zeros on the list
+		while (listZeros.size()>0) {
+
+			// list of keys to be removed from listZeros from each iteration
+			ArrayList<String> listUpdate = new ArrayList<String>(); 
+			
+			// loop through all indexes of 0s
+			Iterator<String> i=listZeros.iterator();
+			while(i.hasNext()) {
+			
+				// transform key into row and column
+				String[] strKey = i.next().split("#");
+				Integer r = Integer.parseInt(strKey[0]);
+				Integer c = Integer.parseInt(strKey[1]);
+				
+				// test if there all possible neighbors are on the the listZeros
+				if((c<columns-1 && !listZeros.contains(r+"#"+(c+1))) ||
+				   (c>0         && !listZeros.contains(r+"#"+(c-1))) ||
+				   (r<rows-1    && !listZeros.contains((r+1)+"#"+c)) ||
+				   (r>0         && !listZeros.contains((r-1)+"#"+c))
+				  ) {
+					// not all neighbors are 0s, so we can update this server
+					// add this index to the list of indexes to be removed from HashSet
+					listUpdate.add(r+"#"+c);
+				} 
+			}
+			
+			if(listUpdate.size()==0)
+				// no possible solution
+				return -1;
+			
+			// remove keys from list of zeros
+			for(String strKey : listUpdate) {
+				listZeros.remove(strKey);
+			}
+			
+			// add an hour
+			numHours++;
+		}
+		
+		// return number of hours
+		return numHours;
+	}
+}
+
+
+
+
